dea7c3b19ed49290eea189b260d34f2f9defbf8f
caribedevelopment-star <caribe.development@gmail.com>
2026-08-31 15:29:52 +0200

    Bridge landing into digital atelier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HomeJourney from "../components/HomeJourney";
 import HomeLoader from "../components/HomeLoader";
 import ImmersiveHomeHero from "../components/ImmersiveHomeHero";
+import HomeThresholdTransition from "../components/HomeThresholdTransition";
 import styles from "./home.module.css";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <main className="pageEnter">
       <HomeLoader />
       <ImmersiveHomeHero />
+      <HomeThresholdTransition />
       <HomeJourney />
 
       <footer className={styles.footer}>
